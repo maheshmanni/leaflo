51d9dd48c9433fba6376f0ee7f414e4b6c6fd68d
maheshmanni <mnm@esi-group.com>
2021-06-07 12:17:19 +0530

    read image

--- app/src/main/java/com/myApps/leaflo/MainActivity.java
+++ app/src/main/java/com/myApps/leaflo/MainActivity.java
@@ -7,27 +7,37 @@ import android.Manifest;
 import android.content.ActivityNotFoundException;
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.Color;
+import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     public  static final int RequestPermissionCode  = 1 ;
+    private Spinner mSpinner;
+    private TextView mTv;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        mSpinner = findViewById(R.id.progress_spin);
+        mTv = findViewById(R.id.percentage_txt);
         EnableRuntimePermission();
         Button btnCamera = findViewById(R.id.capture_btn);
         btnCamera.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
+                mTv.setVisibility(View.INVISIBLE);
                 Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
                 //intent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
 
@@ -40,7 +50,15 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
 
+               // mSpinner.setVisibility(View.VISIBLE);
+             //   mSpinner.animate();
+               int percentage = AverageImg();
+               String str = String.valueOf(percentage);
+                mSpinner.setVisibility(View.GONE);
 
+                mTv.setVisibility(View.VISIBLE);
+                mTv.setText(str + "%");
+               // Toast.makeText(MainActivity.this, str, Toast.LENGTH_LONG).show();
             }
         });
     }
@@ -77,5 +95,40 @@ public class MainActivity extends AppCompatActivity {
                     Manifest.permission.CAMERA}, RequestPermissionCode);
 
         }
+    }
+
+    private int AverageImg()
+    {
+        ImageView iv = findViewById(R.id.image);
+        BitmapDrawable drawable = (BitmapDrawable) iv.getDrawable();
+        Bitmap bitmap = drawable.getBitmap();
+
+        int redBucket = 0;
+        int greenBucket = 0;
+        int blueBucket = 0;
+        int pixelCount = 0;
+
+        for (int y = 0; y < bitmap.getHeight(); y++)
+        {
+            for (int x = 0; x < bitmap.getWidth(); x++)
+            {
+                int c = bitmap.getPixel(x, y);
+
+                pixelCount++;
+                redBucket += Color.red(c);
+                greenBucket += Color.green(c);
+                blueBucket += Color.blue(c);
+                // does alpha matter?
+            }
+        }
+        int averageColor = Color.rgb(redBucket / pixelCount,
+                greenBucket / pixelCount,
+                blueBucket / pixelCount);
+
+        // int red   = (averageColor & 0x00ff0000) >> 16;
+         int green = (averageColor & 0x0000ff00) >> 8;
+       //  int blue  = (averageColor & 0x000000ff);
+        int greenPercentage = (int) ((green / 255.0) * 100);
+        return greenPercentage;
     }
 }
